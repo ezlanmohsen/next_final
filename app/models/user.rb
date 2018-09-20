af5771797cc4111a462f7658fe58fd644a1936ce
ezlan <ezlanmohsen@gmail.com>
2018-09-21 02:25:41 +0800

    Added validation on password in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	has_secure_password
-	
+	validates :password, presence: { on: :create }, length: { minimum: 5, allow_blank: true }
 	validates :email, uniqueness: true
 	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
