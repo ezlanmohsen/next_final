bedc7559862b0fd0223a14ee9e339bfa8cd9fd07
ezlan <ezlanmohsen@gmail.com>
2018-09-20 23:12:00 +0800

    rails g model users. Edited migration file

---
+++ db/migrate/20180920150652_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :occupation
+      t.string :industry
+      t.integer :role, :default => 0 #to use enum in model - [customer, researcher, admin]
+      t.string :email, null: false
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
