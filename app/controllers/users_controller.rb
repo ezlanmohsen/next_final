6db4f03dcb1e1e05508be659c62b08a362f2b35a
ezlan <ezlanmohsen@gmail.com>
2018-09-21 03:10:24 +0800

    created edit form for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,12 +29,21 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		
+		@user = User.find(params[:id])
 	end
+
+	def update
+  		@user = User.find(params[:id])
+	  	if @user.update(user_params)
+	  		redirect_to user_path(@user)
+	  	else
+  			render 'edit'
+  		end
+  end
 
 	private
 
 	def user_params
-  		params.require(:user).permit(:email, :password, :password_confirmation)
+  		params.require(:user).permit!
 	end
 end
