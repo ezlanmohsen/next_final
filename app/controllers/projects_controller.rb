d5af6ed95868fbc1570f78547e7c8a402e485944
ezlan <ezlanmohsen@gmail.com>
2018-09-21 14:30:03 +0800

    Fixed new project form at user#show. Now able to choose from topic table.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,18 +2,13 @@ class ProjectsController < ApplicationController
 	def create
 		@project = Project.new(project_params)
 		@project.user_id = current_user.id
-		@project.topic_id = 1 #placeholder for testing
-
+		# @project.topic_id = 1 #placeholder for testing
 		if @project.save
-			byebug
 			redirect_to user_path(current_user)
 		else
 			redirect_to root_path
-			byebug
 		end
 	end
-
-
 
 	private
 
